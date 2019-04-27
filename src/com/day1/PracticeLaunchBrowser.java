0b858842d7107e6b42bf2efbe30aa63362238a30
d314195 <d314195@W00000R90R6JYM.wg.dir.telstra.com>
2019-04-27 21:30:25 +1000

    assignment

---
+++ src/com/day1/PracticeLaunchBrowser.java
@@ -0,0 +1,42 @@
+package com.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeLaunchBrowser {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\d314195\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver wd = new ChromeDriver();
+		wd.get("https://facebook.com");
+		
+		String displayURL = wd.getCurrentUrl();
+		System.out.println(displayURL);		
+		
+		/*
+		 * wd.navigate().to("https://google.com"); String displaySecondURL =
+		 * wd.getCurrentUrl(); System.out.println(displaySecondURL);
+		 * wd.navigate().back();
+		 */
+		
+		WebElement username = wd.findElement(By.xpath("//input[@type='email'] | //input[@type='phone']"));
+		WebElement password = wd.findElement(By.xpath("//input[@type='password']"));
+		
+		username.sendKeys("r.budhiraja26@gmail.com");
+		username.sendKeys("abc123");	
+		
+		/*
+		 * WebElement sub = wd.findElement(By.xpath("//input[@value='Log In']"));
+		 * sub.click();
+		 */
+		
+		password.sendKeys(Keys.ENTER);
+		wd.wait(10000);
+		wd.quit();
+	}
+
+}
